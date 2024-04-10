c8fd8224db133ff64559adbf0b38c7eac2c3726d
Ffion Haf <ffionhaf33@gmail.com>
2024-04-10 16:31:06 +0100

    feat: add login and register functionality

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,24 +1,53 @@
-from flask import Blueprint, request, jsonify
-from controllers.user import create_user
-# Create a Blueprint for the auth routes
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+from flask import (
+    Blueprint,
+    request,
+    jsonify,
+    render_template,
+    session,
+    redirect,
+    url_for,
+)
+from controllers.user import create_user, verify_user
 
-@bp.route('/register', methods=['POST', 'GET'])
+# Create a Blueprint for the auth routes
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "GET":
-        return 'Hello'
-    # Get data from request
+        return render_template("register.html")
+    # Get data from request - adapted from -> https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
     data = request.form
-    username = data.get('username')
-    password = data.get('password')
-    email = data.get('email')
+    username = data.get("username")
+    password = data.get("password")
+    email = data.get("email")
 
-    if create_user(username=username, password=password, email=email):
-        return jsonify({'message': 'User created successfully'}), 201
-    
-    return jsonify({'message': 'Failed to create user'}), 400
+    if not create_user(username=username, password=password, email=email):
+        return render_template("register.html"), 400
+
+    return redirect(url_for("login"))
 
 
-@bp.route('/health', methods=['GET'])
+@bp.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    # Get data from request - adapted from -> https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
+    data = request.form
+    password = data.get("password")
+    email = data.get("email")
+
+    if not verify_user(email=email, password=password):
+        return render_template("login.html")
+
+    # Add verified user to new session - adapted from -> https://flask.palletsprojects.com/en/3.0.x/quickstart/#sessions
+    session["email"] = email
+
+    return redirect(url_for("home.home"))
+
+
+@bp.route("/health", methods=["GET"])
 def health():
-    return jsonify({'status': True})
+    return jsonify({"status": True})
