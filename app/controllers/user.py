d860309f0ff73bea7d5d5edf361ecd21780cf4fa
Ffion Haf <ffionhaf33@gmail.com>
2024-04-10 16:29:18 +0100

    feat: add password hashing and user verification with argon2

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,5 +1,8 @@
 from models.models import User
 from db.db import Session
+from argon2 import PasswordHasher
+from typing import Optional
+
 
 def create_user(username: str, email: str, password: str) -> bool:
     # Create a new session
@@ -7,16 +10,17 @@ def create_user(username: str, email: str, password: str) -> bool:
 
     # Check if a user with the given username already exists
     existing_user = session.query(User).filter_by(username=username).first()
-    
+
     if existing_user is not None:
         return False
-    
-    # TODO: actually hash password
 
-    new_user = User(username=username, email=email, hashed_password=password)
+    # Hash the password for security
+    ph = PasswordHasher()
+    hashed_pass = ph.hash(password)
+    new_user = User(username=username, email=email, hashed_password=hashed_pass)
 
     try:
-    # Add the new user to the session and commit
+        # Add the new user to the session and commit
         session.add(new_user)
         session.commit()
         return True
@@ -26,3 +30,17 @@ def create_user(username: str, email: str, password: str) -> bool:
         return False
     finally:
         session.close()
+
+
+def verify_user(email: str, password: str) -> Optional[str]:
+    """Verify a user based on email and password combination"""
+    session = Session()
+    user = session.query(User).filter_by(email=email).first()
+    ph = PasswordHasher()
+
+    # verify user supplied password matches stored hash
+    if user and ph.verify(user.hashed_password, password):
+        return user.email
+
+    # user did not pass verification
+    return None
