14a3a981c5f60e75bc8d203bff51190b417632af
Ffion Haf <ffionhaf33@gmail.com>
2024-04-10 15:03:47 +0100

    chore: formatting

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
 import os
+import logging
+import sys
+
 
 def create_app():
     # Create the Flask app
-    app = Flask(__name__, static_folder='static')
+    app = Flask(__name__, static_folder="static")
 
     # Load configurations from environment variables
-    app.config['FLASK_RUN_PORT'] = os.getenv('FLASK_RUN_PORT', 5000)
+    app.config["FLASK_RUN_PORT"] = os.getenv("FLASK_RUN_PORT", 5000)
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.DEBUG)
 
     # Register blueprints
     from .routes import auth, movie, tv, user, home
+
     app.register_blueprint(home.bp)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(movie.bp)
+    app.register_blueprint(tv.bp)
 
     return app
